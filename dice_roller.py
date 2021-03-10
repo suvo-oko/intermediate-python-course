c4781585841cdd600c17a72007646717b421c94a
suvo-oko <miravalles@gmail.com>
2021-03-10 08:00:28 +0100

    Uncommented line 2

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,5 @@
 def main():
-  #print('You rolled a die')
+  print('You rolled a die')
 
-if __name__== "__main__":
+if __name__== "__main__":  # this is an if statement that calls the function main, which will run whenever you run the Python script.
   main()
