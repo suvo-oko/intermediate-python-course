c308349fb8ae73864600d9e0f03c97bfc9807aa5
suvo-oko <miravalles@gmail.com>
2021-03-11 06:18:17 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
+
 def main():
-  print('You rolled a die')
+      dice_rolls = 2
+      dice_sum = 0
+      for i in range(0, dice_rolls):  # for every Index in this range, do this:
+            roll = random.randint(1,6)  # the random.randint function takes 2 integers as parameters, with the beginning and end included in the range. 
+            dice_sum += roll  # this is the same as dice_sum = dice_sum + roll
+            print(f'You rolled a {roll}')
+      print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":  # this is an if statement that calls the function main, which will run whenever you run the Python script.
   main()
